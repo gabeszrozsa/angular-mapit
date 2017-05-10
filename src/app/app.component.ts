f25458f891b98211ce2326af869beb6cd17c9cbd
Gabesz Rózsa <gabesz.rozsa@gmail.com>
2017-05-10 10:23:18 +0200

    display 3 markers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,43 @@ import { Component } from '@angular/core';
 
 export class AppComponent {
   // Start
-  lat: number = 51.678418;
-  lng: number = 7.809007;
+  lat: number = 47.4808722;
+  lng: number = 18.8501225;
+  zoom: number = 10;
 
+  // Markers
+  markers: marker[] = [
+    {
+      name:'Gubacsidülő',
+      lat: 47.453486,
+      lng: 19.098002,
+      draggable: true
+    },
+    {
+      name:'Lágymányos',
+      lat: 47.472983,
+      lng: 19.063669,
+      draggable: true
+    },
+    {
+      name:'Albertfalva',
+      lat: 47.447914,
+      lng: 19.036203,
+      draggable: false
+    },
+  ]
+
+  constructor(){
+
+  }
+
+
+}
+
+// Marker type
+interface marker {
+  name?:string // optional
+  lat: number;
+  lng: number;
+  draggable: boolean;
 }
